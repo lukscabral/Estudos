5b45afeb7c277053711d72ff8f474ae2da348807
lucas <lukscabral95@gmail.com>
2025-01-15 14:33:03 -0300

    finalizado com minimo de estilizaçao

--- Biblioteca/script.js
+++ Biblioteca/script.js
@@ -12,8 +12,8 @@ function adicionar_livro_a_biblioteca(titulo,autor,paginas,status) {
     biblioteca.push(novo_livro);
 }
 
-adicionar_livro_a_biblioteca('Hobbit',"Tolkien",100,'lido');
-adicionar_livro_a_biblioteca('Hobbit 2',"Tolkien",150,'não lido');
+adicionar_livro_a_biblioteca('Hobbit',"Tolkien",100,'Status de leitura: não lido');
+adicionar_livro_a_biblioteca('Hobbit 2',"Tolkien",150,'Status de leitura: não lido');
 
 const lista = document.querySelector('#livros');//lista esta no article
 
@@ -24,12 +24,21 @@ function criar_card(array_livro){
     array_livro.forEach((card_livro, index) => {
         const div_card = document.createElement('div');
         div_card.classList.add('card');
+        const div_btn = document.createElement('div');//div dentro de .card pro btn
+        const div_content = document.createElement('div');
+        div_content.classList.add('info');
 
         const titulo = document.createElement('h3');
         titulo.textContent = 'Titulo: ' + card_livro.titulo;
 
         const autor = document.createElement('p')
         autor.textContent = 'Autor: ' + card_livro.autor;
+
+        const paginas = document.createElement('p');
+        paginas.textContent = 'Numero de paginas: ' + card_livro.paginas;
+
+        const status = document.createElement('p');
+        status.textContent = card_livro.status;
 
         const btn_del = document.createElement('button');
         btn_del.classList.add('deletar');
@@ -38,22 +47,31 @@ function criar_card(array_livro){
         btn_del.addEventListener('click', remover_livro);
 
          // Botão de atualizar com data-index
-         const btn_atualizar = document.createElement('button');
-         btn_atualizar.textContent = 'Atualizar';
-         btn_atualizar.setAttribute('data-index', index); // Adiciona o índice como data attribute
-         btn_atualizar.addEventListener('click', atualizar_livro);
+        const btn_atualizar = document.createElement('button');
+        btn_atualizar.textContent = 'Atualizar';
+        btn_atualizar.setAttribute('data-index', index); // Adiciona o índice como data attribute
+        btn_atualizar.addEventListener('click', atualizar_livro);
 
-        div_card.appendChild(titulo);
-        div_card.appendChild(autor);
-        div_card.appendChild(btn_del);
-        div_card.appendChild(btn_atualizar);
+        const btn_status = document.createElement("button");
+        btn_status.textContent = "Lido / Não lido";
+        btn_status.setAttribute('data-index', index);
+        btn_status.addEventListener('click' , toggle_status);
+        div_card.appendChild(div_content);
+        div_content.appendChild(titulo);
+        div_content.appendChild(autor);
+        div_content.appendChild(paginas);
+        div_content.appendChild(status);
+        div_card.appendChild(div_btn);
+        div_btn.appendChild(btn_status);
+        div_btn.appendChild(btn_del);
+        div_btn.appendChild(btn_atualizar);
 
         fragmento_do_document.appendChild(div_card);
     });
 
     lista.appendChild(fragmento_do_document);// .prepend iria inserir antes do primeiro child
 }
-criar_card(biblioteca);//inicia os pre-existentes
+criar_card(biblioteca);//inicia os cards exemplos pre-existentes
 
 
 const showBtn = document.getElementById("show-dialog");
@@ -91,7 +109,7 @@ btn_criar.addEventListener('click', (e) => {
         modo_edicao = false;
         index_edicao = null;
     } else {
-        adicionar_livro_a_biblioteca(titulo_input, autor_input, paginas_input, 'não lido');
+        adicionar_livro_a_biblioteca(titulo_input, autor_input, paginas_input, 'Status de leitura: não lido');
     }
     criar_card(biblioteca); // Re-renderiza os cards
     dialog.close();
@@ -135,8 +153,16 @@ function adicionar_livro() {
         autor_input = document.getElementById('autor').value;
         paginas_input = document.getElementById('paginas').value;
 
-        adicionar_livro_a_biblioteca(titulo_input,autor_input,paginas_input, 'nao lido ');
+        adicionar_livro_a_biblioteca(titulo_input,autor_input,paginas_input, 'Status de leitura: não lido');
         criar_card(biblioteca);
         dialog.close();
     };
+} 
+
+function toggle_status(e){
+    index_edicao = Number(e.target.getAttribute('data-index'));
+    const livro = biblioteca[index_edicao];
+
+    livro.status = livro.status === 'Status de leitura: lido' ? 'Status de leitura: não lido' : 'Status de leitura: lido' ;
+    criar_card(biblioteca);
 }
